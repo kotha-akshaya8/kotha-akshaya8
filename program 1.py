3e9ea4d20e6f76f6b16e03bbea10347d7b3b10e1
kotha-akshaya8 <k.akshaya1409@gmail.com>
2025-04-02 09:52:36 +0530

    Add files via upload

---
+++ program 1.py
@@ -0,0 +1,102 @@
+python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def calculate_compound_interest(principal,rate,time):
+	"""
+Calculate the compound interest.
+:param principal:The initial amount of money(float or int).
+:param rate:The annual interest rate(float or int).
+:param time:The number of periods the money is invested for(float or int).
+:return:The compound interest(float).
+"""
+	#Convert rate from percentage  to decimal
+	rate_decimal=rate/100
+	#Calculate the compound interest amount=principal*(1+rate_decimal)**time
+	compound_interest=amount-principal
+	return compound_interest
+#Input values
+principal=float(input("Enter the principal amount:"))
+SyntaxError: invalid syntax
+>>> def calculate_compound_interest(principal,rate,time):
+	"""
+Calculate the compound interest.
+:param principal:The initial amount of money(float or int).
+:param rate:The annual interest rate(float or int).
+:param time:The number of periods the money is invested for(float or int).
+:return:The compound interest(float).
+"""
+	#Convert rate from percentage  to decimal
+	rate_decimal=rate/100
+	#Calculate the compound interest amount=principal*(1+rate_decimal)**time
+	compound_interest=amount-principle
+	return compound_interest
+#input values
+principle=float(input("Enter the principal amount:"))
+SyntaxError: invalid syntax
+>>> Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def calculate_compound_interest(principal,rate,time):
+	"""
+Calculate the compound interest.
+:param principal:The initial amount of money(float or int).
+:param rate:The annual interest rate(float or int).
+:param time:The number of periods the money is invested for(float or int).
+:return:The compound interest(float).
+"""
+	#Convert rate from percentage  to decimal
+	rate_decimal=rate/100
+	#Calculate the compound interest amount=principal*(1+rate_decimal)**time
+	compound_interest=amount-principal
+	return compound_interest
+#Input values
+principal=float(input("Enter the principal amount:"))
+SyntaxError: invalid syntax
+>>> def calculate_compound_interest(principal,rate,time):
+	"""
+Calculate the compound interest.
+:param principal:The initial amount of money(float or int).
+:param rate:The annual interest rate(float or int).
+:param time:The number of periods the money is invested for(float or int).
+:return:The compound interest(float).
+"""
+	#Convert rate from percentage  to decimal
+	rate_decimal=rate/100
+	#Calculate the compound interest amount=principal*(1+rate_decimal)**time
+	compound_interest=amount-principle
+	return compound_interest
+#input values
+principle=float(input("Enter the principal amount:"))
+SyntaxError: invalid syntax
+>>> Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def calculate_compound_interest(principal,rate,time):
+	"""
+Calculate the compound interest.
+:param principal:The initial amount of money(float or int).
+:param rate:The annual interest rate(float or int).
+:param time:The number of periods the money is invested for(float or int).
+:return:The compound interest(float).
+"""
+	#Convert rate from percentage  to decimal
+	rate_decimal=rate/100
+	#Calculate the compound interest amount=principal*(1+rate_decimal)**time
+	compound_interest=amount-principal
+	return compound_interest
+#Input values
+principal=float(input("Enter the principal amount:"))
+SyntaxError: invalid syntax
+>>> def calculate_compound_interest(principal,rate,time):
+	"""
+Calculate the compound interest.
+:param principal:The initial amount of money(float or int).
+:param rate:The annual interest rate(float or int).
+:param time:The number of periods the money is invested for(float or int).
+:return:The compound interest(float).
+"""
+	#Convert rate from percentage  to decimal
+	rate_decimal=rate/100
+	#Calculate the compound interest amount=principal*(1+rate_decimal)**time
+	compound_interest=amount-principle
+	return compound_interest
+#input values
+principle=float(input("Enter the principal amount:"))
+SyntaxError: invalid syntax
